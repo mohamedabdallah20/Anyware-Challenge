4ee0e4449afbe985c3126d39555c4200e180ab5b
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-11-10 16:50:42 +0200

    Rename thr hoc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import BurgerMenu from './components/BurgerMenu'
-import withLogger from './hoc/withLogger'
+import withLogger from './hoc/withAuth'
 function App() {
   return (
     <>
